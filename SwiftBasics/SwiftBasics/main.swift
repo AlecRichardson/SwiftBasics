c1758fe569043784a3c1e0b918d4f71bf5398ef3
Alec Richardson <alec@quarkworks.co>
2019-02-02 12:57:25 -0600

    initial commit

---
+++ SwiftBasics/SwiftBasics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftBasics
+//
+//  Created by Alec Richardson on 2/2/19.
+//  Copyright © 2019 Alec Richardson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
